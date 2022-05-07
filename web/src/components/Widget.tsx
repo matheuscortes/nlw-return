1680bbc692033ebd8001e044d331678f1855881c
MatheusCortes <mcortes1245@gmail.com>
2022-05-07 18:05:24 -0300

    Início da construção do widget

---
+++ web/src/components/Widget.tsx
@@ -0,0 +1,31 @@
+import { ChatTeardropDots } from 'phosphor-react'
+import { useState } from 'react'
+import { Popover } from '@headlessui/react'
+
+export function Widget() {
+
+    return(
+        /*
+        Position:
+        Static (standard): The element is positioned according to the normal flow of the document.
+        Relative: The element is positioned according to its original position in the document.
+        Absolute: The element is positioned relative to its closest positioned ancestor, if any; otherwise, 
+        it is placed relative to the document.
+        Fixed: The element ramains in the same position regardless of scrolling.
+        Sticky: The element becomes fixed when it crosses a specific threshold when scrolling. 
+
+        Popover makes our application web accessible
+         */
+        <Popover className='absolute bottom-4 right-4'>
+            <Popover.Panel>Hello World</Popover.Panel>
+           
+            <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
+                <ChatTeardropDots className='w-6 h-6'/>
+
+                <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
+                    Feedback
+                </span>
+            </Popover.Button>            
+        </Popover>
+    )
+}
